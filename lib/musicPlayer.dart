7409b053468e1b814ffedde70e60038b233950d0
Thejana Jayalath <thejanashehan.com@gmail.com>
2025-02-20 22:51:57 +0530

    Add Song model

--- lib/musicPlayer.dart
+++ lib/musicPlayer.dart
@@ -20,3 +20,49 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+class Song {
+  final String title;
+  final String artist;
+  final String imagePath;
+  final String audioPath;
+
+  Song({
+    required this.title,
+    required this.artist,
+    required this.imagePath,
+    required this.audioPath,
+  });
+}
+
+final List<Song> songs = [
+  Song(
+    title: 'Dementia Track-1',
+    artist: 'Dementia_Link',
+    imagePath: 'assets/images/i1.png',
+    audioPath: 'assets/audio/track1.mp3',
+  ),
+  Song(
+    title: 'Dementia Track-2',
+    artist: 'Dementia_Link',
+    imagePath: 'assets/images/i2.png',
+    audioPath: 'assets/audio/track2.mp3',
+  ),
+  Song(
+    title: 'Dementia Track-3',
+    artist: 'Dementia_Link',
+    imagePath: 'assets/images/i3.png',
+    audioPath: 'assets/audio/track3.mp3',
+  ),
+  Song(
+    title: 'Dementia Track-4',
+    artist: 'Dementia_Link',
+    imagePath: 'assets/images/i4.png',
+    audioPath: 'assets/audio/track4.mp3',
+  ),
+  Song(
+    title: 'Dementia special Track',
+    artist: 'Meditational StateHealing Music',
+    imagePath: 'assets/images/i5.jpg',
+    audioPath: 'assets/audio/track5.mp3',
+  ),
+];
