97e3a6b83c7d50adfd1192e41d24bfd85c5a11c1
Thejana Jayalath <thejanashehan.com@gmail.com>
2025-02-20 22:50:46 +0530

    Initial project setup

---
+++ lib/musicPlayer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xFF503663),
+        scaffoldBackgroundColor: const Color(0xFF503663),
+      ),
+      home: const PlaylistScreen(),
+    );
+  }
+}
