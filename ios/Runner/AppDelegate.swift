1e81b39dcc8c8156140025483872badec78a67e9
uchini2003 <uchinimarasinghe561@gmail.com>
2025-03-07 16:33:00 +0530

    updated google maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,14 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBymf4zqU1hy82Jw1gUXI7H1-ZNHCcWOEE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/maps_screen.dart
+++ lib/maps_screen.dart
@@ -1,97 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-class MapsScreen extends StatefulWidget {
-  const MapsScreen({Key? key}) : super(key: key);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-  @override
-  State<MapsScreen> createState() => _MapsScreenState();
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
 }
 
-class _MapsScreenState extends State<MapsScreen> {
-  GoogleMapController? _controller;
-  Location _location = Location();
-  final Set<Marker> _markers = {};
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // Initial camera position (default to a location in Sri Lanka)
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DementiaLink Maps',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MapScreen(),
+    );
+  }
+}
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
   static const CameraPosition _initialPosition = CameraPosition(
     target: LatLng(6.9271, 79.8612), // Colombo, Sri Lanka
     zoom: 14.0,
   );
 
-  @override
-  void initState() {
-    super.initState();
-    _getUserLocation();
-  }
-
-  // Get user's current location
-  void _getUserLocation() async {
-    bool serviceEnabled;
-    PermissionStatus permissionGranted;
-    LocationData locationData;
-
-    // Check if location services are enabled
-    serviceEnabled = await _location.serviceEnabled();
-    if (!serviceEnabled) {
-      serviceEnabled = await _location.requestService();
-      if (!serviceEnabled) {
-        return;
-      }
-    }
-
-    // Check if permission is granted
-    permissionGranted = await _location.hasPermission();
-    if (permissionGranted == PermissionStatus.denied) {
-      permissionGranted = await _location.requestPermission();
-      if (permissionGranted != PermissionStatus.granted) {
-        return;
-      }
-    }
-
-    // Get the current location
-    locationData = await _location.getLocation();
-
-    if (_controller != null && locationData.latitude != null && locationData.longitude != null) {
-      _controller!.animateCamera(
-        CameraUpdate.newLatLng(
-          LatLng(locationData.latitude!, locationData.longitude!),
-        ),
-      );
-
-      // Add a marker for current location
-      setState(() {
-        _markers.add(
-          Marker(
-            markerId: const MarkerId('currentLocation'),
-            position: LatLng(locationData.latitude!, locationData.longitude!),
-            infoWindow: const InfoWindow(title: 'Your Location'),
-          ),
-        );
-      });
-    }
-  }
+  GoogleMapController? _mapController;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Google Maps'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('DementiaLink Maps'),
       ),
       body: GoogleMap(
         initialCameraPosition: _initialPosition,
+        onMapCreated: (controller) {
+          _mapController = controller;
+        },
         myLocationEnabled: true,
         myLocationButtonEnabled: true,
-        markers: _markers,
-        onMapCreated: (GoogleMapController controller) {
-          _controller = controller;
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _getUserLocation,
-        child: const Icon(Icons.my_location),
+        mapType: MapType.normal,
       ),
     );
   }
